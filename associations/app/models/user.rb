e3029609ab94ba2130e822d36bd8765b96605d5d
yonureker <yonureker@gmail.com>
2019-05-29 14:49:36 -0700

    Added Models for Course, User, Enrollment

---
+++ associations/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+  has_one :taught_course,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :Course
+
+end
